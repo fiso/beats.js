0aa5c84238fd15fc7af8836dae0b2fbebb4ae399
Filip Söderholm <filip@happypie.com>
2016-08-09 20:01:00 +0200

    Abandoned work being committed

--- public/js/beats.js
+++ public/js/beats.js
@@ -20,73 +20,73 @@ function Beats(audioUrl, offsetSeconds, bpm) {
 
 Beats.prototype.ready = function() {
     return this._promise;
-}
-
-// Function to identify peaks
-function getPeaksAtThreshold(data, threshold) {
-    var peaksArray = [];
-    var length = data.length;
-    for (var i = 0; i < length;) {
-        if (data[i] > threshold) {
-            peaksArray.push(i);
-            // Skip forward ~ 1/4s to get past this peak.
-            i += 10000;
-        }
-        i++;
-    }
-    return peaksArray;
-}
-
-// Function used to return a histogram of peak intervals
-function countIntervalsBetweenNearbyPeaks(peaks) {
-    var intervalCounts = [];
-    peaks.forEach(function(peak, index) {
-        for (var i = 0; i < 10; i++) {
-            var interval = peaks[index + i] - peak;
-            var foundInterval = intervalCounts.some(function(intervalCount) {
-                if (intervalCount.interval === interval)
-                    return intervalCount.count++;
-            });
-            if (!foundInterval) {
-                intervalCounts.push({
-                    interval: interval,
-                    count: 1
-                });
-            }
-        }
-    });
-    return intervalCounts;
-}
-
-// Function used to return a histogram of tempo candidates.
-function groupNeighborsByTempo(intervalCounts, sampleRate) {
-    var tempoCounts = [];
-    intervalCounts.forEach(function(intervalCount, i) {
-        if (intervalCount.interval !== 0) {
-            // Convert an interval to tempo
-            var theoreticalTempo = 60 / (intervalCount.interval / sampleRate);
-
-            // Adjust the tempo to fit within the 90-180 BPM range
-            while (theoreticalTempo < 90) theoreticalTempo *= 2;
-            while (theoreticalTempo > 180) theoreticalTempo /= 2;
-
-            theoreticalTempo = Math.round(theoreticalTempo);
-            var foundTempo = tempoCounts.some(function(tempoCount) {
-                if (tempoCount.tempo === theoreticalTempo)
-                    return tempoCount.count += intervalCount.count;
-            });
-            if (!foundTempo) {
-                tempoCounts.push({
-                    tempo: theoreticalTempo,
-                    count: intervalCount.count
-                });
-            }
-        }
-    });
-    return tempoCounts;
-}
+};
 
 Beats.prototype.analyzeBpm = function(audioUrl, onComplete) {
+    // Function to identify peaks
+    function getPeaksAtThreshold(data, threshold) {
+        var peaksArray = [];
+        var length = data.length;
+        for (var i = 0; i < length;) {
+            if (data[i] > threshold) {
+                peaksArray.push(i);
+                // Skip forward ~ 1/4s to get past this peak.
+                i += 10000;
+            }
+            i++;
+        }
+        return peaksArray;
+    }
+
+    // Function used to return a histogram of peak intervals
+    function countIntervalsBetweenNearbyPeaks(peaks) {
+        var intervalCounts = [];
+        peaks.forEach(function(peak, index) {
+            for (var i = 0; i < 10; i++) {
+                var interval = peaks[index + i] - peak;
+                var foundInterval = intervalCounts.some(function(intervalCount) {
+                    if (intervalCount.interval === interval)
+                        return intervalCount.count++;
+                });
+                if (!foundInterval) {
+                    intervalCounts.push({
+                        interval: interval,
+                        count: 1
+                    });
+                }
+            }
+        });
+        return intervalCounts;
+    }
+
+    // Function used to return a histogram of tempo candidates.
+    function groupNeighborsByTempo(intervalCounts, sampleRate) {
+        var tempoCounts = [];
+        intervalCounts.forEach(function(intervalCount, i) {
+            if (intervalCount.interval !== 0) {
+                // Convert an interval to tempo
+                var theoreticalTempo = 60 / (intervalCount.interval / sampleRate);
+
+                // Adjust the tempo to fit within the 90-180 BPM range
+                while (theoreticalTempo < 90) theoreticalTempo *= 2;
+                while (theoreticalTempo > 180) theoreticalTempo /= 2;
+
+                theoreticalTempo = Math.round(theoreticalTempo);
+                var foundTempo = tempoCounts.some(function(tempoCount) {
+                    if (tempoCount.tempo === theoreticalTempo)
+                        return tempoCount.count += intervalCount.count;
+                });
+                if (!foundTempo) {
+                    tempoCounts.push({
+                        tempo: theoreticalTempo,
+                        count: intervalCount.count
+                    });
+                }
+            }
+        });
+        return tempoCounts;
+    }
+
     var context = new(window.AudioContext || window.webkitAudioContext)();
     var request = new XMLHttpRequest();
     request.open('GET', audioUrl, true);
