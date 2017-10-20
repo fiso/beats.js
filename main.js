0e67b696e116cd0a321ed5f283316ed188c7cbeb
Filip Haglund <filip@happypie.com>
2017-10-20 11:59:19 +0200

    Cleanup

--- main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var express = require('express');
-var app = express();
-
-app.use(express.static("public"));
-
-app.listen(1338, function () {
-    console.log("Development server running");
-});
